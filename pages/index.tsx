1d06a2e6868358787235fde60a00823e9f96beb6
rarepepi <pepimartinez@protonmail.com>
2023-07-13 17:15:12 -0400

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,10 +47,7 @@ export default function Home() {
           </div>
 
           <div className="flex items-center justify-center flex-1">
-            <NoRampOneClick
-              appId="app_4zd8QoywncK1itHmVhhowU"
-              priceId="price_20iDZj5nETBUOi1i5kOqTa"
-            />
+            <NoRampOneClick priceId="price_20iDZj5nETBUOi1i5kOqTa" />
           </div>
           <div className="">
             {width < 768 ? (
