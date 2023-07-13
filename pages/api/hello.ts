4d5f5b2039180eae174eac0e5c0cc5557bed996e
rarepepi <pepimartinez@protonmail.com>
2023-07-13 04:58:56 -0400

    prices

--- pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
-  res.statusCode = 200;
-
-  res.json({ hello: "world" });
-};
-
-export default handler;
