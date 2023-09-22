83d1e9ab9cf26f6a6083f3acec20ac5d338a21b2
rarepepi <pepimartinez@protonmail.com>
2023-09-21 22:10:23 -0400

    new ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,64 +13,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className="h-screen flex flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-8 px-4 mx-auto">
-          <div className="flex justify-center w-full">
-            {width < 768 ? (
-              <video
-                className="logo h-96"
-                src="/video/mobile_hero.mp4"
-                key="/video/mobile_hero.mp4"
-                autoPlay
-                playsInline
-                loop
-                muted
-              ></video>
-            ) : (
-              <video
-                src="/video/hero.mp4"
-                className="logo"
-                key="/video/hero.mp4"
-                autoPlay
-                playsInline
-                loop
-                muted
-              ></video>
-            )}
-          </div>
-          <div>
-            <p className="font-bold text-lg max-w-sm text-center">
-              ColorsOnChain Testnet by NoRamp is an infinite collection of
-              colors that are generated and stored on-chain for you to use
-              however you’d like.
+          <div className="">
+            <p className="font-bold text-6xl max-w-sm text-center text-white">
+              ColorsOnChain
             </p>
           </div>
 
           <div className="flex items-center justify-center flex-1">
             <NoRampOneClick priceId="price_40xjcBIuVqmwlNyrSp3r9U" testnet />
-          </div>
-          <div className="">
-            {width < 768 ? (
-              <video
-                className="colorGridVideo"
-                src="/video/mobile_color_grid.mp4"
-                key="/video/mobile_color_grid.mp4"
-                autoPlay
-                playsInline
-                loop
-                muted
-              ></video>
-            ) : (
-              <video
-                className="colorGridVideo"
-                src="/video/color_grid.mp4"
-                key="/video/color_grid.mp4"
-                autoPlay
-                playsInline
-                loop
-                muted
-              ></video>
-            )}
           </div>
         </div>
       </main>
